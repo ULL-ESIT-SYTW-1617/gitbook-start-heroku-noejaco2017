bf1897ec348a6eba12b1e95a45a7ede17c5df420
alu0100836059 <alu0100836059@ull.edu.es>
2016-12-27 12:06:13 +0000

    semVer:0.1.41

--- bin/linea-comando-npm.js
+++ bin/linea-comando-npm.js
@@ -433,7 +433,7 @@ if(argv.h || argv.help){
                                               console.log("TAREA GULP");
                                               //añadir las tareas al gulp
                                               //var heroku = require('../node_modules/gitbook-start-plugin-heroku-noejaco2017/linea-comando-heroku');
-                                              var heroku = require('./node_modules/gitbook-start-heroku-noejaco-final/heroku-command.js');
+                                              var heroku = require('./node_modules/gitbook-start-heroku-noejaco-final/heroku-command');
                                               console.log("VARIABLE HEROKU REQUIRE"+heroku);
                                               heroku.initialize(argv.directorio);
 
